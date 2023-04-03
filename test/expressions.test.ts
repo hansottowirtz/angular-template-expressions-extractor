c6f6b3b4f848f9352d99832767add44805f01cc3
Hans Otto Wirtz <hansottowirtz@gmail.com>
2023-04-03 12:09:30 +0200

    fix: optional key access with updated estree parser

--- test/expressions.test.ts
+++ test/expressions.test.ts
@@ -10,7 +10,7 @@ tap.test('test', async (t) => {
   t.same(await p(`<div [thing]="1 | async: 2" />`), ['async(1, 2)']);
   t.same(await p(`<div *thing="a; else b" />`), ['a,"else",b']);
   t.same(await p(`<div [thing]="a?.b?.c" />`), ['a?.b?.c']);
-  // t.match(await p(`<div [thing]="a?.b?.[0]" />`), ["a?.b?.[0]"]);
+  t.match(await p(`<div [thing]="a?.b?.[0]" />`), ["a?.b?.[0]"]);
   t.same(await p(`<div (thing)="action(1, 2)" />`), ['action(1, 2)']);
   t.same(
     await p(`
